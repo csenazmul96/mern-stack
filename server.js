686b781a6875168f3d8a6e8553efc68e04c3cf26
nazmul <csenazmul96@gmail.com>
2024-12-19 23:16:34 +0600

    directory structure

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const connectDB = require('./config/dbConnection');
 const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
-const app = express();
+const server = express();
 
 // Connect to the database
 connectDB();
 
 // Middleware
-app.use(express.json());
+server.use(express.json());
 
 // Routes
-app.use('/api/users', userRoutes);
+server.use('/api/users', userRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
